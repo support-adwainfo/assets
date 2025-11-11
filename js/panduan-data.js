86e0e6fe2e2818b54cd69682e24ba914c449e756
Abdul Rohman <abdulrohman@adwainfo.net>
2025-11-11 16:31:17 +0700

    panduan data

---
+++ js/panduan-data.js
@@ -0,0 +1,58 @@
+const panduan_data = [
+    {
+        title: "JUKNIS PSE PUBLIK KOMDIGI",
+        file: "JUKNIS PSE PUBLIK KOMDIGI.pdf"
+    },
+    {
+        title: "SOP Pendaftaran PSE Lingkup Publik",
+        file: "20251106 SOP Pendaftaran PSE Lingkup Publik.pdf"
+    },
+    {
+        title: "Format Surat Tugas Pejabat Pendaftar PSE Lingkup Publik Yang Berasal Dari Institusi",
+        file: "FORMAT SURAT TUGAS PEJABAT PENDAFTAR PSE LINGKUP PUBLIK YANG BERASAL DARI INSTITUSI.pdf"
+    },
+    {
+        title: "Format Surat Tugas Pejabat Pendaftar PSE Lingkup Publik Yang Berasal Dari Instansi",
+        file: "FORMAT SURAT TUGAS PEJABAT PENDAFTAR PSE LINGKUP PUBLIK YANG BERASAL DARI INSTANSI.pdf"
+    },
+    {
+        title: "Format Surat Permohonan Pemutusan Akses Oleh Kementerian Lembaga Aparat Penegak Hukum Dan Atau Lembaga Peradilan",
+        file: "FORMAT SURAT PERMOHONAN PEMUTUSAN AKSES OLEH KEMENTERIAN_LEMBAGA APARAT PENEGAK HUKUM DAN_ATAU LEMBAGA PERADILAN.pdf"
+    },
+    {
+        title: "Format Surat Permohonan Pemutusan Akses Oleh Masyarakat",
+        file: "FORMAT SURAT PERMOHONAN PEMUTUSAN AKSES OLEH MASYARAKAT.pdf"
+    },
+    {
+        title: "Format Surat Permohonan Normalisasi Sistem Elektronik Lingkup Publik Instansi Dan Institusi",
+        file: "FORMAT SURAT PERMOHONAN NORMALISASI SISTEM ELEKTRONIK LINGKUP PUBLIK INSTANSI DAN INSTITUSI.pdf"
+    },
+    {
+        title: "Format Surat Keterangan Sistem Elektronik Tidak Digunakan",
+        file: "FORMAT SURAT KETERANGAN SISTEM ELEKTRONIK TIDAK DIGUNAKAN.pdf"
+    },
+    {
+        title: "Kepmen Kominfo 519 2024 Ekosistem PDN",
+        file: "Kepmen Kominfo 519 2024 Ekosistem PDN.pdf"
+    },
+    {
+        title: "PP 71 2019 PSTE",
+        file: "PP 71 2019 PSTE.pdf"
+    },
+    {
+        title: "PM Komdigi 5 2025 PSE Lingkup Publik",
+        file: "PM Komdigi 5 2025 PSE Lingkup Publik.pdf"
+    },
+    {
+        title: "UU 19 2016 Perubahan ITE",
+        file: "UU 19 2016 Perubahan ITE.pdf"
+    },
+    {
+        title: "UU 1 2024 Perubahan Kedua ITE",
+        file: "UU 1 2024 Perubahan Kedua ITE.pdf"
+    },
+    {
+        title: "UU 11 2008 ITE",
+        file: "UU 11 2008 ITE.pdf"
+    }
+];
